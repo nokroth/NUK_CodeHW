693d84ec529be7c35fd975355ebfd2e417bc8cae
johnnychiu <johnnychiu0817@gmail.com>
2025-10-21 00:53:25 +0800

    Check

--- College_1/test/2024_reverseandadd.cpp
+++ College_1/test/2024_reverseandadd.cpp
@@ -1,32 +1,40 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> // 引入所有標準函式庫
 
 using namespace std;
 
-bool que(string x){
-    int i = 0, j = x.size()-1;
-    while (i < j){
-        if (x[i] != x[j]) return false;
-        i++;
-        j--;
-    }
-    return true;
+// 你的迴文判斷函數，功能是正確的
+bool is_palindrome(string x){
+    string original = x;
+    reverse(x.begin(), x.end());
+    return original == x;
 }
 
 int main(){
+    // ========== 加速 I/O ==========
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // ============================
+
     int t;
     cin >> t;
     while (t--){
         string s;
-        int cnt = 0;
         cin >> s;
-        while (!que(s)){
+        int cnt = 0;
+
+        while (!is_palindrome(s)){
             cnt++;
-            string rs = s;
-            reverse(s.begin(), s.end());
-            long long int ans = stoi(s)+stoi(rs);
-            s = to_string(ans);
+            string reversed_s = s;
+            reverse(reversed_s.begin(), reversed_s.end());
+
+            // 使用 stoll 來處理大數字
+            long long sum = stoll(s) + stoll(reversed_s);
+
+            s = to_string(sum);
         }
+
         cout << cnt << ' ' << s << '\n';
-        
     }
+
+    return 0;
 }
